32d74bef42dbb52e5c20f0bd111c69a142961b21
tejasri15 <51012931+tejasri15@users.noreply.github.com>
2019-07-27 18:17:03 +0530

    display the html file

---
+++ index.js
@@ -0,0 +1,47 @@
+// var url = require('url');
+// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+// //Parse the address:
+// var q = url.parse(adr, true);
+
+// /*The parse method returns an object containing url properties*/
+// console.log(q.host);
+// console.log(q.pathname);
+// console.log(q.search);
+
+// /*The query property returns an object with all the querystring parameters as properties:*/
+// var qdata = q.query;
+// console.log(qdata.month);
+
+const getScript = (url) => {
+    return new Promise((resolve, reject) => {
+        const http      = require('http'),
+              https     = require('https');
+
+        let client = http;
+
+        if (url.toString().indexOf("https") === 0) {
+            client = https;
+        }
+
+        client.get(url, (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                resolve(data);
+            });
+
+        }).on("error", (err) => {
+            reject(err);
+        });
+    });
+};
+
+(async (url) => {
+    console.log(await getScript(url));
+})('https://stackoverflow.com/questions/6287297/reading-content-from-url-with-node-js');
